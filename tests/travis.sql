75d84c6f5f9c99292548acd2576c72de5ac5cd10
Joseph Mancuso <idmann509@gmail.com>
2018-04-24 14:36:46 -0400

    added travis support

---
+++ tests/travis.sql
@@ -0,0 +1,19 @@
+# Create Testuser
+CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
+GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
+# Create DB
+CREATE DATABASE IF NOT EXISTS `demo` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `demo`;
+# Create Table
+CREATE TABLE IF NOT EXISTS `subscriptions` (
+  `user_id` int(11) NOT NULL,
+  `plan` varchar(50) DEFAULT NULL,
+  `plan_id` varchar(50) DEFAULT NULL,
+  `plan_name` varchar(150) DEFAULT NULL,
+  `trial_ends_at` timestamp NULL DEFAULT NULL,
+  `ends_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+# Add Data
